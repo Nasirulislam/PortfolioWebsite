2a4293bc5a66bcb072858b540c2014c0d942d22a
waheed <waheedsaeed053@gmail.com>
2023-01-04 12:48:38 +0500

    updated build

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,4 +1,4 @@
-const url = "http://35.162.202.5:5000";
+// const url = "http://35.162.202.5:5000";
 // const url = "http://192.168.10.23:5000";
-
+const url = "http://52.199.85.143/api";
 export default url;
