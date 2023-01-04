1ee4d937cd4603a02f72a7c26ba7ed2b92f00b38
waheed <waheedsaeed053@gmail.com>
2023-01-04 15:26:36 +0500

    updated build

--- src/components/admin/NavBar.js
+++ src/components/admin/NavBar.js
@@ -5,7 +5,6 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import logo from "./../../Assets/images/4.jpg";
 import { BiLogOut } from "react-icons/bi";
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
