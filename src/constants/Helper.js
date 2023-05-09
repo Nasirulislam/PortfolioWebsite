047573b189201f2ab2adb7a3232f11fd406c197e
moiz2511 <moizahmad25112gmail.com>
2023-05-09 16:13:25 +0500

    canvas scaling

--- src/constants/Helper.js
+++ src/constants/Helper.js
@@ -14,11 +14,19 @@ export default function Helper({ banner, largeCircle, index, imageFluid }) {
         <>
             {
                 show ?
-                    <motion.img
-                        className={"bottom-img "+(imageFluid ? "img-fluid" : "")}
-                        src={image.src}
-                        animate={{ x: largeCircle.x, y: largeCircle.y }}
-                        key={index}
+                    // <motion.img
+                    //     className={"bottom-img "+(imageFluid ? "img-fluid" : "")}
+                    //     src={image.src}
+                    //     animate={{ x: largeCircle.x, y: largeCircle.y }}
+                    //     key={index}
+                    // />
+                    <Blurhash
+                        hash={banner.hash}
+                        width={parseInt(banner.width)}
+                        height={parseInt(banner.height)}
+                        resolutionX={32}
+                        resolutionY={32}
+                        punch={1}
                     />
                     : <Blurhash
                         hash={banner.hash}
