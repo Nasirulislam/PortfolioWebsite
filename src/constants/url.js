b980f67216323e7a329189f60fd3bbd77e3ed040
Abdul Basit <abhiawan1227@gmail.com>
2023-08-10 19:26:36 +0500

    new

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,8 +1,6 @@
 // const url = "http://35.162.202.5:5000";
 // const url = "http://52.199.85.143:5000";
 // const url = "http://52.199.85.143:5000";
-const url = "https://dex.davidellis.sco.uk/api";
+const url = "https://dex.davidellis.co.uk/api";
 // const url = "http://localhost:5000";
 export default url;
-
-//
