709c6a045bafda945f5f195d9054811459c4cf6e
Abdul Basit <abhiawan1227@gmail.com>
2023-08-11 12:41:46 +0500

    new

--- src/components/Home/HomeIndex.js
+++ src/components/Home/HomeIndex.js
@@ -209,7 +209,7 @@ function HomeIndex(props) {
     // const IMAGE_WIDTH = imgElement.clientWidth;
     // const IMAGE_HEIGHT = imgElement.clientHeight;
     const ZOOM_SPEED = window.innerWidth <= 500 ? 80 : 75; // Lower is faster
-    const ZOOM_BREAKPOINT = 10; // When it should stop zooming in
+    const ZOOM_BREAKPOINT = window.innerWidth <= 500 ? 6.5 : 9.5; // When it should stop zooming in
     // console.log('ZOOM ZOOM', ZOOM_BREAKPOINT)
     // const IMAGE_HEIGHT_MAX = IMAGE_HEIGHT * ZOOM_BREAKPOINT;
     const ABSOLUTE = ZOOM_BREAKPOINT * ZOOM_SPEED; // Absolute position, when the Element reached maximum size
